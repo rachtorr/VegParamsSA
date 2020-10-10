6df04e8f156dbeef277b9fc2b4cc329d6e3269d4
Rachel Torres <r_torres@ucsb.edu>
2020-10-10 16:46:47 -0700

    changed defs

--- IOworkflows/IOworkflow_E_copy.R
+++ IOworkflows/IOworkflow_E_copy.R
@@ -2,27 +2,22 @@
 library(RHESSysIOinR)
 library(tidyverse)
 library(sensitivity)
-source("~/RHESSysIOinR/R/select_output_variables_R.R")
-setwd("~/VegParamsSA/scripts")
-
-# new output folder: 
-#"~/../../scratch/ratorres/" 
-
-## put nice in front of command - gives it less priority 
+source("~/RHESSysIOinR-master/R/select_output_variables_R.R")
+setwd("~/Google Drive File Stream/My Drive/patches/scripts")
 
 # 1: set up input files and command line options
 # RHESSys Inputs
 input_rhessys <- list()
-input_rhessys$rhessys_version <- "~/RHESSys/bin/rhessys7.2"
+input_rhessys$rhessys_version <- "~/RHESSys-naomitest2/bin/rhessys7.2"
 input_rhessys$tec_file <- "../tecfiles/tec.coast"
 input_rhessys$world_file <- "../worldfiles/OakPatch.world.0"
 input_rhessys$world_hdr_prefix <- "../worldfiles/OakPatch.hdr"
 input_rhessys$flow_file <- "../flowtables/OakPatch.flow"
 input_rhessys$start_date <- "1947 10 1 1"
-input_rhessys$end_date <- "2007 10 2 2"
-input_rhessys$output_folder <- "~/../../scratch/ratorres/out"
+input_rhessys$end_date <- "2020 10 2 2"
+input_rhessys$output_folder <- "../out/RHESSysIOinR_output"
 input_rhessys$output_filename <- "evergreen.sob"
-input_rhessys$command_options <- c("-b -g -climrepeat")
+input_rhessys$command_options <- c("-b -g -c -climrepeat")
 
 # HDR (header) file
 input_hdr_list <- list()
@@ -37,33 +32,35 @@ input_hdr_list$base_stations <- c("../clim/mission_base")
 # sample input def list - can change parameters
 # [1] on stratum_def corresponds to veg file listed above
 # parameter method LHC - c(low, high, n=number of runs)
-n = 300
+n = 68
+#nsp = 1250
+input_def_list <- list()
 input_def_list <- list()
 input_def_list <- list(
   list(input_hdr_list$soil_def[1], "pore_size_index", c(0.55,0.7, n)),
   list(input_hdr_list$soil_def[1], "psi_air_entry", c(0.05, 3, n)),
-  list(input_hdr_list$stratum_def[1], "epc.leaf_cn", c(30.0125, 65, n)),
+  list(input_hdr_list$stratum_def[1], "epc.leaf_cn", c(30.15, 50, n)),
   list(input_hdr_list$stratum_def[1], "epc.branch_turnover", c(0.01, 0.04, n)),
-  list(input_hdr_list$stratum_def[1], "epc.gl_smax", c(0.0025, 0.004, n)),
+  list(input_hdr_list$stratum_def[1], "epc.gl_smax", c(0.0025, 0.0039, n)),
   list(input_hdr_list$stratum_def[1], "epc.flnr_age_mult", c(1.00025, 1.99975, n)),
   list(input_hdr_list$stratum_def[1], "epc.litter_moist_coef", c(5.00125e-05, 0.00069985, n)),
   list(input_hdr_list$stratum_def[1], "epc.psi_close", c(-9, -3, n)),
   list(input_hdr_list$stratum_def[1], "mrc.q10", c(1.8, 2.2, n)),
   list(input_hdr_list$stratum_def[1], "epc.vpd_close", c(2500.875, 5993.125, n)),
-  list(input_hdr_list$stratum_def[1], "epc.leaf_turnover", c(0.12, 0.425, n)),
-  list(input_hdr_list$stratum_def[1], "epc.froot_turnover", c(0.2, 0.7, n)),
+  list(input_hdr_list$stratum_def[1], "epc.leaf_turnover", c(0.1200, 0.2345, n)),
+  list(input_hdr_list$stratum_def[1], "epc.froot_turnover", c(0.3, 0.6, n)),
   list(input_hdr_list$stratum_def[1], "epc.storage_transfer_prop", c(0.475, 0.575, n)),
   list(input_hdr_list$stratum_def[1], "epc.height_to_stem_coef", c(0.1501875, 0.9492, n)),
-  list(input_hdr_list$stratum_def[1], "epc.waring_pa", c(0.05, 0.8, n)),
-  list(input_hdr_list$stratum_def[1], "epc.root_growth_direction", c(0.5, 1.5, n)),
-  list(input_hdr_list$stratum_def[1], "epc.root_distrib_parm", c(2.5, 4.25, n)),
-  list(input_hdr_list$stratum_def[1], "epc.proj_sla", c(6, 13, n)),
+  list(input_hdr_list$stratum_def[1], "epc.waring_pa", c(0.1168, 0.63, n)),
+  list(input_hdr_list$stratum_def[1], "epc.root_growth_direction", c(0.5066, 1.48, n)),
+  list(input_hdr_list$stratum_def[1], "epc.root_distrib_parm", c(5, 20, n)),
+  list(input_hdr_list$stratum_def[1], "epc.proj_sla", c(7, 12.5, n)),
   list(input_hdr_list$stratum_def[1], "epc.alloc_stemc_leafc", c(1.3, 1.8, n)),
   list(input_hdr_list$stratum_def[1], "epc.ext_coef", c(0.650025, 0.749975, n)),
   list(input_hdr_list$stratum_def[1], "specific_rain_capacity", c(0.000100025, 0.000199975, n)),
   list(input_hdr_list$stratum_def[1], "epc.flnr_sunlit", c(0.1, 0.638134193125, n)),
   list(input_hdr_list$stratum_def[1], "epc.flnr_shade", c(0.090801719375, 0.397329453125, n)),
-  list(input_hdr_list$stratum_def[1], "epc.netpabs_sunlit", c(0.1, 0.439022910625, n)),
+  list(input_hdr_list$stratum_def[1], "epc.netpabs_sunlit", c(0.1, 0.373, n)),
   list(input_hdr_list$stratum_def[1], "epc.netpabs_shade", c(0.1, 0.4, n)),
   list(input_hdr_list$stratum_def[1], "epc.netpabs_age_mult", c(1.00025, 1.99975, n)),
   list(input_hdr_list$stratum_def[1], "epc.cpool_mort_fract", c(0.001, 0.05, n)),
@@ -72,7 +69,7 @@ input_def_list <- list(
   list(input_hdr_list$stratum_def[1], "epc.waring_pb", c(0.05, 1.0, n)),
   list(input_hdr_list$stratum_def[1], "epc.max_storage_percent", c(0.2, 0.4, n)),
   list(input_hdr_list$stratum_def[1], "epc.min_leaf_carbon", c(0.00102, 0.02, n)),
-  list(input_hdr_list$stratum_def[1], "epc.resprout_leaf_carbon", c(0.005, 0.01, n)),
+  list(input_hdr_list$stratum_def[1], "epc.resprout_leaf_carbon", c(0.0052, 0.01, n)),
   list(input_hdr_list$stratum_def[1], "epc.alloc_frootc_leafc", c(0.3, 1.2, n)),
   list(input_hdr_list$stratum_def[1], "epc.frootc_crootc", c(0.7, 1.25, n))
 )
@@ -83,15 +80,21 @@ output_variables <- data.frame(out_file=character(), variable=character(), strin
 output_variables[1,] <- data.frame("bd", "lai", stringsAsFactors=FALSE)
 output_variables[2,] <- data.frame("bd", "height", stringsAsFactors = FALSE)
 output_variables[3,] <- data.frame("bd", "plantc", stringsAsFactors = FALSE)
-output_variables[11,] <- data.frame("bd", "rootdepth", stringsAsFactors=FALSE)
+output_variables[4,] <- data.frame("bd", "rootdepth", stringsAsFactors = FALSE)
 
-output_variables[4,] <- data.frame("cdg", "leafc", stringsAsFactors=FALSE)
-output_variables[5,] <- data.frame("cdg", "live_stemc", stringsAsFactors=FALSE)
-output_variables[6,] <- data.frame("cdg", "live_crootc", stringsAsFactors=FALSE)
-output_variables[7,] <- data.frame("cdg", "dead_stemc", stringsAsFactors = FALSE)
-output_variables[8,] <- data.frame("cdg", "dead_crootc", stringsAsFactors=FALSE)
-output_variables[9,] <- data.frame("cdg", "dead_leafc", stringsAsFactors = FALSE)
-output_variables[10,] <- data.frame("cdg", "leafc_store", stringsAsFactors=FALSE)
+output_variables[5,] <- data.frame("cdg", "leafc", stringsAsFactors=FALSE)
+output_variables[6,] <- data.frame("cdg", "dead_leafc", stringsAsFactors = FALSE)
+output_variables[7,] <- data.frame("cdg", "leafc_store", stringsAsFactors=FALSE)
+output_variables[8,] <- data.frame("cdg", "live_stemc", stringsAsFactors=FALSE)
+output_variables[9,] <- data.frame("cdg", "live_crootc", stringsAsFactors=FALSE)
+output_variables[10,] <- data.frame("cdg", "dead_stemc", stringsAsFactors = FALSE)
+output_variables[11,] <- data.frame("cdg", "dead_crootc", stringsAsFactors=FALSE)
+
+output_variables[12,] <- data.frame("bd", "trans", stringsAsFactors=FALSE)
+output_variables[13,] <- data.frame("cdg", "cpool", stringsAsFactors=FALSE)
+output_variables[14,] <- data.frame("cdg", "psn_to_cpool", stringsAsFactors=FALSE)
+
+
 
 #########################################################################
 # make tec file - neeed to add part from generate_input_files
@@ -153,8 +156,8 @@ if (is.null(input_def_list[1]) == FALSE){
     # Attach group ID to option_sets_def_par
     tmp <- seq_along(1:nrow(run_sob$X))
     sobol_tmp <- cbind(run_sob$X, group_id=tmp)
-    option_sets_def_par <- list(sobol_tmp[,c(1:2,29)], 
-                                sobol_tmp[,3:29])
+    option_sets_def_par <- list(sobol_tmp[,c(1:2,36)], 
+                                sobol_tmp[,3:36])
 
     names(option_sets_def_par) <- input_def_file_unique
 }
@@ -171,6 +174,7 @@ option_sets_all <- make_all_option_table(parameter_method,
                                          option_sets_def_par,
                                          option_sets_standard_par=NULL,
                                          option_sets_dated_seq)
+
 
 nrow(option_sets_all)
 # command line parameters
@@ -194,11 +198,12 @@ n = nrow(option_sets_def_par[[1]])
 # then takes output and puts into new file in directory 'allsim'
 # each time params change, the def file is written over, not saved. Each run will have an output that should correspond with ID params in row of csv
 
+  
 # start here to run
 if(nrow(option_sets_def_par[[1]])==nrow(option_sets_all)){
 
-  saveRDS(run_sob, "../out/soboljansenoutput.rds")
-  write.csv(option_sets_all, file.path(input_rhessys$output_folder, paste(input_rhessys$output_filename, "_all_options.csv", sep="")), row.names = FALSE, quote=FALSE)
+  saveRDS(run_sob, "../out/RHESSysIOinR_output/allsim/jansenoutput.rds")
+  write.csv(option_sets_all, paste("../out/RHESSysIOinR_output/allsim/option_sets", input_rhessys$output_filename, sep="_"), row.names = FALSE, quote=FALSE)
 
   option_sets_rhessys_rows <- nrow(option_sets_all)
 
@@ -228,27 +233,27 @@ if(nrow(option_sets_def_par[[1]])==nrow(option_sets_all)){
                         tec_file = "../tecfiles/tec.spinup",
                         flow_file = option_sets_all$flow_file[bb],
                         start_date = option_sets_all$start_date[bb],
-                        end_date = "2012 10 1 1",
+                        end_date = "2010 10 1 1",
                         output_file = paste(option_sets_all$output_folder[bb],"/", option_sets_all$output_filename[bb], sep=""),
                         input_parameters = "-s 0.036294 359.485800 -sv 0.036294 359.485800 -gw 0.346205 0.416299",
                         command_options = "-b -g -c -climrepeat")
 
-      #   cmd = sprintf("mv ../worldfiles/OakPatch.world.0.Y2007M10D1H1.state ../worldfiles/OakWorld.spun60")
-      # system(cmd)
-      # 
-      # print(paste("Spin up complete"))
-      # 
-      # # for run with output
-      # rhessys_command(rhessys_version = option_sets_all$rhessys_version[bb],
-      #                 world_file = "../worldfiles/OakWorld.spun60",
-      #                 world_hdr_file = "../worldfiles/OakPatch.hdr",
-      #                 tec_file = "../tecfiles/tec.coast",
-      #                 flow_file = option_sets_all$flow_file[bb],
-      #                 start_date = "2002 9 30 1",
-      #                 end_date = "2012 10 1 1",
-      #                 output_file = paste(option_sets_all$output_folder[bb],"/", option_sets_all$output_filename[bb], sep=""),
-      #                 input_parameters = "-s 0.036294 359.485800 -sv 0.036294 359.485800 -gw 0.346205 0.416299",
-      #                 command_options = "-b -g -c -climrepeat")
+        cmd = sprintf("mv ../worldfiles/OakPatch.world.0.Y2007M10D1H1.state ../worldfiles/OakWorld.spun60")
+      system(cmd)
+
+      print(paste("Spin up complete"))
+
+      # for run with output
+      rhessys_command(rhessys_version = option_sets_all$rhessys_version[bb],
+                      world_file = "../worldfiles/OakWorld.spun60",
+                      world_hdr_file = "../worldfiles/OakPatch.hdr",
+                      tec_file = "../tecfiles/tec.coast",
+                      flow_file = option_sets_all$flow_file[bb],
+                      start_date = "2005 9 30 1",
+                      end_date = "2025 10 1 1",
+                      output_file = paste(option_sets_all$output_folder[bb],"/", option_sets_all$output_filename[bb], sep=""),
+                      input_parameters = "-s 0.036294 359.485800 -sv 0.036294 359.485800 -gw 0.346205 0.416299",
+                      command_options = "-b -g -c -climrepeat")
 
 
         # Process RHESSys output
@@ -265,6 +270,10 @@ if(nrow(option_sets_def_par[[1]])==nrow(option_sets_all)){
   )
 }
 
+  
+  
+  
+  
   
   
 
