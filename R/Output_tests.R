83aa5cd03e17a9cc6b259a453ddcbf268d87b3a8
rachtorr <r_torres@ucsb.edu>
2021-05-11 18:31:10 +0000

    updated output file and quag param runs'

--- R/Output_tests.R
+++ R/Output_tests.R
@@ -1,102 +1,53 @@
-setwd("../out/")
-setwd("~/Documents/patches/out")
+setwd("VegParamsSA/R/tree_outputs/")
 library(RHESSysIOinR)
 library(tidyverse)
 library(reshape2)
 library(gridExtra)
 library(zoo)
-source("../R/readin_selected_vars.R")
+
+code="QUAG"
+spn = 81
 
 ### load in David's data 
-vis <- read.csv("/Volumes/GoogleDrive/My Drive/plant_params/DM_data/tree_and_turfgrass_monroe2_selected_weightedmeanVIs.csv")
+vis <- read.csv("../../data/tree_and_turfgrass_monroe2_selected_weightedmeanVIs.csv")
+lai_ind <- read.csv("../../data/alonzo_data_df.csv")
 
 ### select species 
-quag_data <- vis %>% dplyr::filter(sp_code==
-                                     "PLRA")
+quag_data <- vis %>% dplyr::filter(sp_code==code)
+lai_data <- lai_ind %>% dplyr::filter(class==spn & 
+                                        !is.na(lai) &
+                                        fractional_cov>0.7)
+lai_data <- lai_data[!lai_data %in% boxplot.stats(lai_data$carbobperarea)$out,]
+  
 ### load in RHESSys output 
-vars = c("lai",
-         "height",
-         "plantc")
- 
-### this won't work 
-quag <- readin_selected_vars(vars=vars, dir="~/Documents/patches/out/RHESSysIOinR_output/allsim/")
-quag <- read.csv("../R/tree_outputs/plra_outputs.csv")
+quag <- readRDS(paste(tolower(code), "_outputs.rds", sep=""))
 length(unique(quag$run))
 
-p <- read.csv("RHESSysIOinR_output/allsim/evergreen.sob_all_options.csv")
-head(quag)
+p <- read.csv("evg_broadleaf_params.csv")
 
-### check it out 
-boxplot(quag$lai)
-
-quag %>% 
-  dplyr::filter(year == 2011 | year == 2014 | year ==2017) %>% 
-  dplyr::filter(month==6 | month == 7) %>% 
+quag_mean <- quag %>% dplyr::filter(year == 2011 | year == 2014 | year ==2017) %>% 
   group_by(year, run) %>% 
-  summarize_at(vars(lai), funs(mean)) %>%
-  ggplot() + geom_density(aes(x=lai, col=as.factor(year)))
-  
-
-### to plot timeseries
-mean <- quag %>% dplyr::filter(year>2004) %>% 
-  group_by(date) %>% 
   summarize_at(vars(lai), funs(mean))
 
-stats <- quag %>%  dplyr::filter(year>2004) %>% 
-  dplyr::group_by(date) %>% 
+density_plot <- ggplot(quag_mean) + geom_density(aes(x=lai, col=as.factor(year)))
+  
+##############################################################
+### to plot timeseries
+mean_df <- quag %>% 
+  group_by(wy) %>% 
+  summarize_at(vars(lai), funs(mean))
+
+stats <- quag %>%  
+  dplyr::group_by(wy) %>% 
   dplyr::summarize(quant_first = quantile(lai, probs = c(0.25)), quant_third = quantile(lai, probs = c(0.75)))
 
 ## plot
 timeser <- quag %>% 
-  dplyr::filter(year>1990) %>% 
-  ggplot() + geom_line(aes(x=date, y=lai), col='grey') + 
-  geom_line(data=mean, aes(x=date, y=lai), col='red') + 
-  geom_line(data=stats, aes(x=date, y=quant_first, group=1), col='blue', linetype='dashed') +
-  geom_line(data=stats, aes(x=date, y=quant_third, group=1), col='blue', linetype='dashed')
+  ggplot() + geom_line(aes(x=wy, y=lai, group=run), col='grey') + 
+  geom_line(data=mean_df, aes(x=wy, y=lai, group=1), col='red') + 
+  geom_line(data=stats, aes(x=wy, y=quant_first, group=1), col='blue', linetype='dashed') +
+  geom_line(data=stats, aes(x=wy, y=quant_third, group=1), col='blue', linetype='dashed')
 timeser
-
-## check out time series for all runs for 20 years 
-annlai1 <- quag %>% 
-  group_by(year, run) %>% 
-  summarize_at(vars(lai), funs(max)) %>%
-  dplyr::filter(year==2011) %>%
-  dplyr::filter(lai > low_lai & lai < high_lai) 
-
-annlai2 <- quag[quag$run %in% annlai1$run,] %>% 
-  dplyr::filter(year == 2011 | year == 2014 | year ==2017) %>% 
-  group_by(year, run) %>% 
-  summarize_at(vars(lai), funs(mean)) %>%
-  dcast(run ~ year) %>%
-  dplyr::filter(`2014`<`2011`)
-
-annlai <- quag[quag$run %in% annlai2$run,]
-
-filt_lai <- annlai %>% 
-  group_by(year, run) %>% 
-  summarize_at(vars(lai), funs(mean)) %>% 
-  ggplot() + geom_line(aes(x=year, y=lai, col=as.factor(run)), show.legend = F) + 
-  geom_vline(aes(xintercept=2011)) +
-  geom_vline(aes(xintercept=2014)) + 
-  geom_vline(aes(xintercept=2017))
-filt_lai
-
-### summarize annual lai for 2011, 2014, 2017 
-meann <- quag %>% 
-  dplyr::filter(year == 2011 | year == 2014 | year ==2017) %>% 
-  dplyr::filter(month==6 | month == 7) %>% 
-  group_by(year, run) %>% 
-  summarize_at(vars(lai), funs(mean))
-
-maxx <- annlai %>%  dplyr::filter(year == 2011 | year == 2014 | year ==2017) %>% 
-  dplyr::filter(month==6 | month == 7) %>% 
-  group_by(year, run) %>% 
-  summarize_at(vars(lai), funs(max))
-  
-## plot
-ggplot(meann) + geom_density(aes(x=lai, col=as.factor(year)))
-ggplot(maxx) + geom_density(aes(x=lai, col=as.factor(year)))
-
-
 
 ######################################################
 ######################################################
@@ -114,25 +65,21 @@ nd <- ggplot(ndvi) + geom_density(aes(x=elevn, col='11')) +
   geom_density(aes(x=fourtn, col='14')) + 
   geom_density(aes(x=seventn, col='17')) + 
   ggtitle("change in NDVI for QUAG (n=50)")
-nd
 
 ######################################################
 ######################################################
 # look at extent of changes for entire parameter set 
 # filter by the change in ndvi 
 
-mean_yrs <- dcast(meann, run ~ year, value.var = 'lai')
+mean_yrs <- dcast(quag_mean, run ~ year, value.var = 'lai')
 colnames(mean_yrs) <- c("run","pre","dur","post")
 ggplot(mean_yrs) + geom_jitter(aes(x=pre, y=post, col=dur)) + geom_abline(aes(intercept=0, slope=1))
-
-## try to filter param sets to those that match changes 
 
 mean_yrs$decline <- (mean_yrs$pre-mean_yrs$dur)/mean_yrs$pre
 mean_yrs$recover <- (mean_yrs$pre-mean_yrs$post)/mean_yrs$pre
 head(mean_yrs)
-boxplot(mean_yrs$decline)
 
-int <- list(2*sd(ndvi$decline), 2*sd(ndvi$recover))
+int <- list(sd(ndvi$decline), sd(ndvi$recover))
 
 ### filter by difference in LAI between 2011-2014 (decline) and 2011-2017 (recover)
 rh_filter <- as.data.frame(mean_yrs) %>% 
@@ -140,16 +87,15 @@ rh_filter <- as.data.frame(mean_yrs) %>%
                 mean_yrs$decline > mean(ndvi$decline)-int[[1]] &    mean_yrs$recover < mean(ndvi$recover)+int[[2]] &    mean_yrs$decline > mean(ndvi$recover)-int[[2]])
 nrow(rh_filter)
 
-ggplot(rh_filter) + geom_density(aes(x=decline, col='decline')) + geom_density(aes(x=recover, col='recover'))
-
 rh <- ggplot(rh_filter) + geom_density(aes(x=pre, col='11')) +
   geom_density(aes(x=dur, col='14')) +
   geom_density(aes(x=post, col='17')) +
   ggtitle(paste("change in LAI (n=",nrow(rh_filter),")"))
-rh
 
 grid.arrange(nd, rh)
-################################33
+################################
+tosave <- quag[quag$run %in% rh_filter$run,]
+write_rds(tosave, "quag_outputs.rds")
 
 rh_melt <- rh_filter %>% dplyr::select(-decline, -recover) %>% melt( id.var='run')
 
@@ -170,22 +116,21 @@ x = nrow(quag_data)/3
 rh11 <- rh_filter %>% dplyr::select(pre) %>% sample_n(x) 
 quag11 <- cbind(rh11, nd11)
 ## plot
-ggplot(quag11) + geom_point(aes(x=pre, y=NDVI))
+# ggplot(quag11) + geom_point(aes(x=pre, y=NDVI))
 
 rh14 <- rh_filter %>% dplyr::select(dur) %>% sample_n(x) 
 quag14 <- cbind(rh14, nd14)
 ## plot 
-ggplot(quag14) + geom_point(aes(x=dur, y=NDVI))
+# ggplot(quag14) + geom_point(aes(x=dur, y=NDVI))
 
 rh17 <- rh_filter %>% dplyr::select(post) %>% sample_n(x) 
 quag17 <- cbind(rh17, nd17)
 ## plot 
-ggplot(quag17) + geom_point(aes(x=post, y=NDVI))
+# ggplot(quag17) + geom_point(aes(x=post, y=NDVI))
 
 ## qqplot 
 qqplot(quag17$post, quag17$NDVI)
 qqnorm(rh17$post)
-
 
 ## plot sample of parameter sets
 ggplot() + geom_density(data=rh11, aes(x=pre, col='11')) +
@@ -193,8 +138,10 @@ ggplot() + geom_density(data=rh11, aes(x=pre, col='11')) +
   geom_density(data=rh17, aes(x=post, col='17'))
 
 ### check % changes 
-pre <- sample_n(rh_filter, x) %>% dplyr::select(decline)
-post <- sample_n(rh_filter, x) %>% dplyr::select(recover)
+tmp <- sample_n(rh_filter, x) 
+tmp <- rh_filter
+pre <- tmp %>% dplyr::select(decline)
+post <- tmp %>% dplyr::select(recover)
 pre <- as.matrix(pre)
 post <- as.matrix(post)
 ## plots
@@ -213,31 +160,29 @@ decline <- as.data.frame(cbind(model=pre, rs=pre_rs))
 recover <- as.data.frame(cbind(model=post, rs=post_rs))
 
 ## plot density of both model, data, pre and post
-ggplot() + geom_density(data=decline, aes(x=decline,col='model')) +
-  geom_density(data=decline, aes(x=rs,col='rs')) +
-  geom_density(data=recover, aes(x=recover,col='model'), linetype='dashed') +
-  geom_density(data=recover, aes(x=rs,col='rs'), linetype='dashed') 
+ggplot() + geom_density(data=decline, aes(x=decline,col='model',linetype='pre')) +
+  geom_density(data=decline, aes(x=rs,col='rs',linetype='pre')) +
+  geom_density(data=recover, aes(x=recover,col='model', linetype='post')) +
+  geom_density(data=recover, aes(x=rs,col='rs', linetype='post')) 
 
 ## look at density of model only 
-ggplot() + geom_density(data=decline, aes(x=decline,col='model')) +
-  #geom_density(data=decline, aes(x=rs,col='rs')) +
-  geom_density(data=recover, aes(x=recover,col='model'), linetype='dashed') 
-  
+ggplot() + geom_density(data=as.data.frame(pre), aes(x=decline,col='model')) +
+  geom_density(data=decline, aes(x=rs,col='rs')) 
 
 ## plot boxplots of both model and data (pre)
 ggplot() + 
-  geom_boxplot(aes(x="model", y=pre$decline, fill='model'), col='grey', alpha=0.75) + 
-  geom_boxplot(aes(x="rs data", y=pre_rs, fill='rs'), col='grey', alpha=0.75) +
-  geom_jitter(aes(x="model", y=pre$decline)) +
-  geom_jitter(aes(x="rs data", y=pre_rs)) +
-  geom_boxplot(aes(x="model-post", y=post$recover)) +
-  geom_boxplot(aes(x="data-post", y=post_rs))
+  geom_boxplot(data=decline, aes(x="model-decline", y=decline, fill='model'), col='grey') + 
+  geom_boxplot(data=decline, aes(x="data-decline", y=rs, fill='rs'), col='grey') +
+  geom_boxplot(data=recover, aes(x="model-post", y=recover, fill='model')) +
+  geom_boxplot(data=recover, aes(x="data-post", y=rs, fill='rs')) 
 
 ### pre is change from 2011-2014 
 ### post is change from 2011-2017
 median(pre)
+#median(pre$decline)
 median(pre_rs)
 median(post)
+#median(post$recover)
 median(post_rs)
   
 ### t.test for whenever that seems like the next step 
@@ -250,12 +195,47 @@ wilcox.test(pre, pre_rs, alternative="two.sided", paired=FALSE, conf.int = T)
 
 wilcox.test(post, post_rs, alternative="two.sided", paired=FALSE, conf.int = T, conf.level = 0.95)
 
+## LAI 
+## compare mean LAI in 2011 for model (pre drought conditions)
+
+ggplot(lai_data) + geom_boxplot(aes(x="data", y=lai)) +
+  geom_boxplot(data=rh_filter, aes(x="model", y=pre))
+
+ggplot(lai_data) + geom_density(aes(x=lai, col='data')) + 
+  geom_density(data=rh_filter, aes(x=pre, col='model'))
+
+wilcox.test(rh_filter$pre, lai_data$lai, alternative="two.sided", paired=FALSE, conf.int = T, conf.level = 0.95)
+
+n=100
+modlai = sample_n(as.data.frame(rh_filter$pre), n)
+modlai = sort(modlai$`rh_filter$pre`)
+datlai = sample_n(as.data.frame(lai_data$lai), n)
+datlai = sort(datlai$`lai_data$lai`)
+cortest = as.data.frame(cbind(model = modlai,
+                        data = datlai))
+## correlation with Kendall's tau 
+cor.test(cortest$model, cortest$data, method="kendall", alternative="two.sided")
+## this is similar to a qqplot 
+ggplot(cortest) + 
+  geom_point(aes(x=data,y=model)) +
+  geom_abline(aes(slope=1, intercept=0))
+
+## using Kendall's rank test for correlation 
+## non-parametric 
+## Kendall’s Tau: usually smaller values than Spearman’s rho correlation. Calculations based on concordant and discordant pairs. Insensitive to error. P values are more accurate with smaller sample sizes 
+# https://www.statisticssolutions.com/kendalls-tau-and-spearmans-rank-correlation-coefficient/
+# also Spearman method gives a warning message 'Cannot compute exact p-value with ties'
+
+
+
+
 ### Wilcoxon test null hypothesis is equal medians.
 ### If null is rejected, there is evidence that one of the distributions is shifted to the left or right, and the medians of the two populations differ, also called a 'location shift' 
 ### Wilcoxon statistic W is number of times the values from one distribution are less than the second distribution in a matrix (in this case a 50x50 matrix)
 ### when including conf.int=T, computes measure of how distributions differ with 95% confidence interval. The difference in location is estimated as the median of the difference between a sample from x and a sample from y.
 
 ### bootstrapping is another non-parameteric approach for non-normal data 
+
 
 
 ### write draft of this section for methods 
