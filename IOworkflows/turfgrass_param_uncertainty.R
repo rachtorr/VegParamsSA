79deec8b3081581981e0dd74e7850d8eff474cc6
Rachel Torres <r_torres@ucsb.edu>
2023-10-07 15:47:42 -0700

    turfgrass spinup

--- IOworkflows/turfgrass_param_uncertainty.R
+++ IOworkflows/turfgrass_param_uncertainty.R
@@ -14,7 +14,7 @@
 # 3 patch / 2 stratum / 25% pavement 
 # -- test_msr_no_irr_3fam.world (need to change areas)
 
-setwd("~/VegParamsSA/scripts/")
+setwd("~/Desktop/VegParamsSA/scripts/")
 library(RHESSysIOinR)
 library(tidyverse)
 library(sensitivity)
@@ -29,17 +29,17 @@ rhv = "/Users/rtorres/RHESSys-rt/rhessys/rhessys7.4"
 tec= "../tecfiles/tec.spinup"
 str = "1989 10 1 1"
 end = "1999 10 1 1"
-cmd = "-g -vmort_off -of watbal_of.yml -v -6 -climrepeat"
+cmd = "-g -vmort_off -of watbal_of.yml -v -6 -climrepeat -msr"
 
 # what's being changed 
-world = c("../worldfiles/test_msr_no_irr_2stratum.world",
-          "test_msr_no_irr_tgrass-only.world")
-hdr = c("test_msr_tgC4.hdr", "test_msr_tgC3.hdr")
-
+world_tgrass = "../worldfiles/test_msr_no_irr_tgrass-only.world"
+world_fam = "../worldfiles/test_msr_no_irr_3patchfam.world"
+#hdr = c("test_msr_tgC4.hdr", "test_msr_tgC3.hdr")
+hdr = "../worldfiles/test_msr_tgC3.hdr"
 
 # saving here
-outdir = "../out/tgrass50/"
-msr_flag="off"
+outdir = "../out/"
+msr_flag="on"
 
 for (bb in seq_along(1:n)){
   
@@ -51,16 +51,56 @@ for (bb in seq_along(1:n)){
   
     print(paste("----------------- Run", bb ,"of", n, "-----------------"))
 
+    # spin up the grass 
     rhessys_command(rhessys_version = rhv,
-                    world_file = world,
-                    world_hdr_file = "../worldfiles/test_msr.hdr",
+                    world_file = world_tgrass,
+                    world_hdr_file = hdr,
                     tec_file = tec,
                     flow_file = "../flowtables/test_msr.flow",
                     start_date = str,
                     end_date = end,
-                    output_file = "../out/msr_tests",
+                    output_file = outdir,
                     input_parameters = "-s 0.0363 359.486 -sv 0.0363 359.486 -gw 0.346 0.416",
-                    command_options = cmd)
+                    command_options = cmd) 
+    
+    # take output worldfiles and re-do as 3fam 
+    data_list <- list()
+    
+    # Open the file for reading
+    file <- "../worldfiles/test_msr_no_irr_tgrass-only.world.Y1995M9D30H1.state"
+    con <- file(file, "r")
+    
+    # Initialize an empty data frame to store the current block of data
+    current_data <- data.frame()
+    
+    line <- readLines(con)
+    # Split the line into two columns based on whitespace
+    parts <- strsplit(trimws(line), "\\s+")
+      
+    # get variables from worldfile
+    
+    
+    # save variables in the other worldfile with patch fam 3
+    
+    # refer to awk if needed 
+    
+    # Close the file
+    close(con)
+    
+    # Combine the data frames into a single data frame
+    final_data <- do.call(rbind, data_list)
+    
+    
+    rhessys_command(rhessys_version = rhv,
+                    world_file = world_fam,
+                    world_hdr_file = hdr,
+                    tec_file = "../tecfiles/tec.coast",
+                    flow_file = "../flowtables/test_msr.flow",
+                    start_date = str,
+                    end_date = end,
+                    output_file = outdir,
+                    input_parameters = "-s 0.0363 359.486 -sv 0.0363 359.486 -gw 0.346 0.416",
+                    command_options = cmd) 
     
     # save basin output 
     output <- read.csv("../out/wb_bas.csv")
