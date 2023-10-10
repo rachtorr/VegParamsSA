e15a268c227e27c7bd82d8efa5519d48863fc8d8
Rachel Torres <r_torres@ucsb.edu>
2023-10-10 14:14:44 -0700

    updated patch water balance

--- R/surface_msr_helper_funcs.R
+++ R/surface_msr_helper_funcs.R
@@ -20,3 +20,60 @@ change_wf_area <- function(worldfile, pct_area_list, patchids){
   print(paste("changed", patchids, "area in worldfile to", pct_area_list))
   
 }
+
+
+watbal_wrapper = function(file){
+  tmp = read.csv("~/VegParamsSA/out/wb_bas.csv")
+  bas = tmp %>% mutate(canopy_snow_stored = snow_stored,
+                 canopy_rain_stored = rain_stored) %>% 
+    watbal_basin_of() %>% 
+    mkdate() 
+  
+  plot = ggplot(bas) + geom_point(aes(x=date, y=watbal))
+  
+  return(list(df = bas, plot=plot))
+}
+
+# function for running water balance for single patch
+patch_balance_of = function (pd){
+  
+  pd$trans_sat = pd$transpiration_sat_zone
+  pd$trans_unsat = pd$transpiration_unsat_zone
+  qouta = ifelse(pd$streamflow > 0, pd$streamflow, pd$Qout)
+  pd$watbal.tmp = with(pd, rain_throughfall + Qin - 
+                         qouta -  
+                         trans_sat - 
+                         trans_unsat - 
+                         evaporation - evaporation_surf)
+  pd$sd = with(pd, sat_deficit - 
+                 rz_storage - 
+                 unsat_storage)
+  pd$weighted_snow_stored = pd$snow_stored 
+  pd$weighted_rain_stored = pd$rain_stored 
+  tmp = pd %>% group_by(zoneID, hillID, basinID, patchID, day, 
+                        month, year) %>% summarize(can_snow_stored = sum(weighted_snow_stored), 
+                                                   can_rain_stored = sum(weighted_rain_stored))
+  pd = left_join(pd, tmp[, c("basinID", "hillID", "zoneID", 
+                             "patchID", "day", "month", "year", "can_snow_stored", 
+                             "can_rain_stored")])
+  pd$can_water_stored = pd$can_rain_stored + pd$can_snow_stored
+  tmp = diff(pd$sd)
+  tmp = c(0, tmp)
+  pd$sddiff = tmp
+  tmp = diff(pd$snow_throughfall)
+  tmp = c(0, tmp)
+  pd$snodiff = tmp
+  tmp = diff(pd$detention_store)
+  tmp = c(0, tmp)
+  pd$detdiff = tmp
+  tmp = diff(pd$litter.rain_stored)
+  tmp = c(0, tmp)
+  pd$litdiff = tmp
+  tmp = diff(pd$can_water_stored)
+  tmp = c(0, tmp)
+  pd$candiff = tmp
+  pd$watbal = with(pd, watbal.tmp + sddiff - snodiff - detdiff - 
+                     litdiff - candiff)
+  pd$watbal[1] = 0
+  return(pd)
+}
